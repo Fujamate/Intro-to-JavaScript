408e8bfa9405829580863d79654f465a811d3ca4
Kevin Hölscher <kevin-hoelscher@gmx.de>
2023-08-14 12:55:50 +0200

    array added for the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = "Falafel";
-
-document.write(favoriteFood)
+let pokemonList = [
+    { name: 'Pikachu', height: 0.4, types: 'Electro'},
+    { name: 'Charmander', height: 0.6, types: 'Fire'},
+    { name: 'Bulbasaur', height: 0.7, types: 'Grass'},
+    { name: 'Arbok', height: 3.5, types: 'Poisen'},
+]
