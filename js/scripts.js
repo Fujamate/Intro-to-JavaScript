608f6b694ba01c5ffe3b1482ebfd158281e47ef1
Kevin Hölscher <kevin-hoelscher@gmx.de>
2023-08-15 02:33:10 +0200

    added the forEach loop for task 1 (1.5)

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,26 @@ let pokemonList = [
     { name: 'Arbok', height: 3.5, types: 'Poisen'},
 ]
 
+pokemonList.forEach (item =>  {
+   if (item.height > 2) {
+    document.write(`<p>${item.name} is over 2 m tall</p> `)
+ } else {
+    document.write(`<p>${item.name} (height: ${item.height}) </p> `)
+ }})
  
- for (let i = 0; i < pokemonList.length; i++) {
+ /* for (let i = 0; i < pokemonList.length; i++) {
 
-    /*-------------- Part 1 is commented for visual reasons  --------------------*/
+    -------------- Part 1 is commented for visual reasons  --------------------
 
-    /* document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) </p> `) */
+    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) </p> `)
 
 
-    /* IF the height is above 2 then write the special text. else just print the object */
+    IF the height is above 2 then write the special text. else just print the object
    if (pokemonList[i].height > 2) {
     document.write(`<p>${pokemonList[i].name} is over 2 m tall</p> `)
  } else {
     document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) </p> `)
  }
- }
+ } */
 
  
