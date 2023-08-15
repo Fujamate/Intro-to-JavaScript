773c76d44c34e28f1c2dd2c78a8c29f771938c78
Kevin Hölscher <kevin-hoelscher@gmx.de>
2023-08-15 03:05:04 +0200

    added the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,43 @@
-let pokemonList = [
-    { name: 'Pikachu', height: 0.4, types: 'Electro'},
-    { name: 'Charmander', height: 0.6, types: 'Fire'},
-    { name: 'Bulbasaur', height: 0.7, types: 'Grass'},
-    { name: 'Arbok', height: 3.5, types: 'Poisen'},
-]
+let pokemonRepository =(function () {
+   let pokemonList = [
+      { name: 'Pikachu', height: 0.4, types: 'Electro'},
+      { name: 'Charmander', height: 0.6, types: 'Fire'},
+      { name: 'Bulbasaur', height: 0.7, types: 'Grass'},
+      { name: 'Arbok', height: 3.5, types: 'Poisen'},
+   ]
 
-pokemonList.forEach (item =>  {
-   if (item.height > 2) {
-    document.write(`<p>${item.name} is over 2 m tall</p> `)
- } else {
-    document.write(`<p>${item.name} (height: ${item.height}) </p> `)
- }})
+   function getAll () {
+      return pokemonList;
+   }
+
+   function add (pokemon) {
+      pokemonList.push(pokemon)
+   }
+
+   return {
+      getAll: getAll,
+      add: add
+   }
+   
+   
+   
+})()
+
+pokemonRepository.getAll().forEach( item => { if (item.height > 2) {
+      document.write(`<p>${item.name} is over 2 m tall</p> `)
+   } else {
+      document.write(`<p>${item.name} (height: ${item.height}) </p> `)
+   }})
+   
+/* pokemonList.forEach (item =>  {
+      if (item.height > 2) {
+      document.write(`<p>${item.name} is over 2 m tall</p> `)
+   } else {
+      document.write(`<p>${item.name} (height: ${item.height}) </p> `)
+   }}) */
+
  
+   
  /* for (let i = 0; i < pokemonList.length; i++) {
 
     -------------- Part 1 is commented for visual reasons  --------------------
