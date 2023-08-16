9c4d1ba2a022c0a11ccf8b2a9bbcc9c6aa15f83c
Kevin Hölscher <kevin-hoelscher@gmx.de>
2023-08-16 11:54:52 +0200

    deleted the comments

--- js/scripts.js
+++ js/scripts.js
@@ -29,28 +29,5 @@ pokemonRepository.getAll().forEach( item => { if (item.height > 2) {
       document.write(`<p>${item.name} (height: ${item.height}) </p> `)
    }})
    
-/* pokemonList.forEach (item =>  {
-      if (item.height > 2) {
-      document.write(`<p>${item.name} is over 2 m tall</p> `)
-   } else {
-      document.write(`<p>${item.name} (height: ${item.height}) </p> `)
-   }}) */
-
- 
-   
- /* for (let i = 0; i < pokemonList.length; i++) {
-
-    -------------- Part 1 is commented for visual reasons  --------------------
-
-    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) </p> `)
-
-
-    IF the height is above 2 then write the special text. else just print the object
-   if (pokemonList[i].height > 2) {
-    document.write(`<p>${pokemonList[i].name} is over 2 m tall</p> `)
- } else {
-    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) </p> `)
- }
- } */
 
  
